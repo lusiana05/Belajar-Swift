0857bbdac71c6925edc01b273cc5419a59e9c645
Lusiana Ilmanastri Oktiviasusanti <ilmanastri@gmail.com>
2022-08-20 00:49:14 +0700

    Update

--- Data Diri/main.swift
+++ Data Diri/main.swift
@@ -7,5 +7,23 @@
 
 import Foundation
 
-print("Hello, World!")
+//Form data diri
+print("Welcome to Dicoding Academy")
 
+print("Masukkan nama depan Anda:"); let firstName = readLine()!
+print("Masukkan nama belakang Anda:"); let lastName = readLine()!
+print("Masukkan alamat Anda:"); let address = readLine()!
+print("Masukkan pekerjaan Anda:"); let job = readLine()!
+print("Masukkan umur Anda:"); let age = readLine()!
+
+//Menggabungkan nama
+let fullName = firstName + " " + lastName
+
+print("------------------------------")
+
+//Print program
+print("Did you know \(fullName)?")
+print("\(firstName) is an \(job)")
+print("She is \(age) years old and lived in \(address).")
+
+print("------------------------------")
